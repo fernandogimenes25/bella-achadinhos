ebf0543a892b1f4029daa3fc2dca5878dd1c24ef
Fernando Gimenes <fernando@gmail.com>
2026-06-17 16:55:14 -0300

    force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client";export const dynamic = 'force-dynamic';
 
 import { useState, useEffect } from "react";
 import { getProducts, getCategories, type Product } from "./lib/supabase";
